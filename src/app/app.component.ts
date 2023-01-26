0dde4e07f1bab60cb29c9ceac7358b9fa8b2665f
userAugustos <felipe_augustos84@outlook.com.br>
2023-01-26 10:37:24 -0300

    feat: finished the operation of drag and drop around the items of the towers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,31 +8,46 @@ import { Component, ElementRef, ViewChild, Renderer2, ViewChildren, QueryList } 
 
 // const
 export class AppComponent {
-	colors = ["#3f92ff", "#46ff3c", "#ffff0b", "#fc6a08", "#fc333c", "#6d4d93"] // 6 colors
-
-	constructor(private renderer: Renderer2, private el: ElementRef){}
-
-  // @ViewChildren('tower') towers!: QueryList<ElementRef<HTMLElement>>;
-
-  async ngOnInit() {
-    const towers = this.el.nativeElement.querySelectorAll('.tower')
+	colors = ["#3f92ff", "#46ff3c", "#ffff0b", "#fc6a08", "#fc333c", "#6d4d93"]
+  towers: HTMLDivElement[] = []
+  draggedItem: any;
+	constructor(private renderer: Renderer2, private el: ElementRef){
+  }
+  ngOnInit() {
+    this.towers = this.el.nativeElement.querySelectorAll('.tower')
     this.colors.forEach(color => {
-      // i was doing this just like i was with vanilla js, but this is a better approach
+      // I was doing this just like i was with vanilla js, but this is a better approach
       const span = this.renderer.createElement('span');
       this.renderer.addClass(span,  'item')
       this.renderer.setAttribute(span, 'draggable', 'true')
-      towers[0].appendChild(span)
+      this.towers[0].appendChild(span)
+    })
+    this.towers.forEach(tower => {
+      this.renderer.listen(tower, 'dragover', (event) => { this.dragOver(event) })
+      this.renderer.listen(tower, 'drop', (event) => { this.dragDrop(event, tower) })
     })
   }
-	async ngAfterViewInit() {
+  ngAfterViewInit() {
     const items: HTMLSpanElement[] = this.el.nativeElement.querySelectorAll('.item')
-
 		items.forEach((item, i) => {
-      console.debug(item)
 			this.renderer.setStyle(item, 'width', `${i+4}rem`)
-			this.renderer.setStyle(item, 'background-color', `${this.colors[i]}`)
-
-			// item.nativeElement.addEventListener()
+			this.renderer.setStyle(item, 'background-color', `${this.colors[i]}`);
+      this.renderer.listen(item, 'dragstart', () => { this.dragStart(item) })
 		})
 	}
+
+  dragStart(item: HTMLSpanElement) {
+    this.draggedItem = item
+    console.debug(this.draggedItem)
+  }
+  dragOver(event: DragEvent) {
+    event.preventDefault();
+    console.debug(this.draggedItem)
+  }
+  dragDrop(event: DragEvent, tower: HTMLDivElement) {
+    // event.preventDefault();
+    console.log("drag dropped");
+    tower.prepend(this.draggedItem);
+    this.draggedItem = null;
+  }
 }
